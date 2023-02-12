9f90d110a13ec200221f83d48d656eff2dccec37
Jonas Glad <jpg@axacon.dk>
2023-02-12 14:31:51 +0100

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'modules/screens/home_page_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     EasyLocalization(
