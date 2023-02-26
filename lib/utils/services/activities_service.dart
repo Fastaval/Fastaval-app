8b64b3cf6103a411f6170d70c83bae9884ffd900
Mads Heimdal Thy <mads@heimdal-thy.dk>
2023-02-26 20:37:17 +0100

    Fix bug with date time on Program page

---
+++ lib/utils/services/activities_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:fastaval_app/config/models/activity_item.dart';
+import 'package:fastaval_app/config/models/program.dart';
+import 'package:fastaval_app/constants/app_constants.dart';
+import 'package:http/http.dart' as http;
+
+Future<Program> fetchProgram() async {
+  var response = await http.get(Uri.parse('$baseUrl/app/v3/activities/*'));
+
+  if (response.statusCode == 200) {
+    return Program.fromJson(jsonDecode(response.body));
+  }
+
+  throw Exception('Failed to load program');
+}
+
+Future<List<ActivityItem>> getDay(String isoDate) async {
+  var url = Uri.parse('$baseUrl/app/v3/activities/$isoDate');
+
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return (jsonDecode(response.body) as List)
+        .map((item) => ActivityItem.fromJson(item))
+        .toList();
+  } else {
+    throw Exception('Failed to download programs');
+  }
+}
