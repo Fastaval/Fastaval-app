3d27a5b2d00f8714419b28edfcb8ec89165bbb2d
Jonas Glad <jonas.p.glad3@gmail.com>
2023-02-26 18:22:41 +0100

    background now A  constant

--- lib/modules/screens/infoscreen.dart
+++ lib/modules/screens/infoscreen.dart
@@ -582,19 +582,7 @@ class InfoScreen extends StatelessWidget {
               Container(
                 height: double.infinity,
                 width: double.infinity,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFFFF9800),
-                      Color(0xFFFB8c00),
-                      Color(0xFFF57C00),
-                      Color(0xFFEF6c00),
-                    ],
-                    stops: [0.1, 0.4, 0.7, 0.9],
-                  ),
-                ),
+                decoration: fastaBoxDecoration,
               ),
               SizedBox(
                 height: double.infinity,
