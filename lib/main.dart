0c8f4f0faefc53f95a6bc82ab966a8f489f4b2f1
Jonas Glad <jonas.p.glad3@gmail.com>
2023-02-26 17:21:54 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 // import 'package:firebase_core/firebase_core.dart';
 // import 'firebase_options.dart';
 
+import 'firebase_options.dart';
 import 'modules/screens/home_page_view.dart';
 
 @pragma('vm:entry-point')
