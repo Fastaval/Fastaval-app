01791d5dbcde548d546956244ba4879e821f3034
Mads Heimdal Thy <mads@heimdal-thy.dk>
2023-03-17 22:08:38 +0100

    Be able to handle foreground notifications

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,15 @@ void main() async {
         fallbackLocale: const Locale('da'),
         child: const MyApp()),
   );
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
 }
 
 class MyApp extends StatelessWidget {
